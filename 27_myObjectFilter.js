1814e309a16f943d72f6bfad0228a3e8415ce52b
Corentin SAMSON <corentin@samson.one>
2023-01-05 15:13:44 +0100

    edit 27, add 29 and 30

--- 27_myObjectFilter.js
+++ 27_myObjectFilter.js
@@ -1,12 +1,5 @@
 function objectFilter(users) {
-    return users.filter((user) => {return user.age >= 18;}).map(function(user) {return `${user.firstName} ${user.lastName}`;});
+    return users.filter((user) => {return user.age >= 18;}).map(function(user) {return `${user.surname} ${user.name}`;});
 }
-
-// console.log(objectFilter([
-//   { firstName: "John", lastName: "Doe", age: 30 },
-//   { firstName: "Jane", lastName: "Doe", age: 20 },
-//   { firstName: "Bob", lastName: "Smith", age: 15 },
-//   { firstName: "Alice", lastName: "Johnson", age: 25 }
-// ]));
 
 module.exports = objectFilter;
